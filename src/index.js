abbce2bc7e31fbbb47b836baa3ad1c5641fa863a
angel <angelkendall13@gmail.com>
2023-06-07 21:33:19 +1000

    Updated to use createRoot from react-dom/client

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-ReactDOM.render(
+// This is the recommended approach in React 18
+createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
